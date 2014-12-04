0644bc850e7815dc07bd2e817449be0b433a1f0e
Vlad <askadias@mail.ru>
2014-12-04 19:28:50 +0300

    updated gradle script to include groovy sources

--- service/grails-app/utils/velocity/DBCache.groovy
+++ service/grails-app/utils/velocity/DBCache.groovy
@@ -4,7 +4,7 @@ package velocity
  * Created by Tiger on 25.09.14.
  */
 class DBCache {
-    List<VelocityConfig> configs;
+    List<VelocityConfig> configs
 
     void invalidate() {
         configs = VelocityConfig.all
